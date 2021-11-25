16bf0091b76a64edcbd71612a580882c641cb434
Jimmy Bastos <jimmybastosdev@gmail.com>
2021-11-25 10:47:28 -0300

    🔖 Rebuild 3.6.1

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-jar uf ./lib/plugpag-3.6.1/classes.aar ./src
+jar uvf ./lib/plugpag-3.6.1/classes.jar ./src/*
 mkdir -p ./3.x/android/br/com/uol/pagseguro/plugpag/3.6.1
 cp ./src/plugpag-3.6.1.pom ./3.x/android/br/com/uol/pagseguro/plugpag/3.6.1
-zip -r ./3.x/android/br/com/uol/pagseguro/plugpag/3.6.1/plugpag-3.6.1.arr ./lib/plugpag-3.6.1/*
+zip -r ./3.x/android/br/com/uol/pagseguro/plugpag/3.6.1/plugpag-3.6.1.aar ./lib/plugpag-3.6.1/*
